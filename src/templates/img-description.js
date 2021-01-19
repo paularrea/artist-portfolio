230a2bdf83d9b3f9208799b9897fe1fc47dfd2cc
Pau Larrea <paularrea12@hotmail.com>
2021-01-19 13:18:17 +0100

    gatsby update deploy

--- src/templates/img-description.js
+++ src/templates/img-description.js
@@ -4,9 +4,9 @@ import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import styles from "./gallery.template.module.scss"
 import UvTemplate from "../components/UvToggle/uvTemplate"
-import ArrowLeft from '@material-ui/icons/ArrowLeft';
-import ArrowRight from '@material-ui/icons/ArrowRight';
-import './icons.css'
+import ArrowLeft from "@material-ui/icons/ArrowLeft"
+import ArrowRight from "@material-ui/icons/ArrowRight"
+import "./icons.css"
 
 const ImgDescription = props => {
   console.log(props.pageContext)
@@ -21,21 +21,30 @@ const ImgDescription = props => {
         <div className={styles.container}>
           <div className={styles.arrows_container}>
             <div className={styles.arrow}>
-              {prev && <Link to={`/gallery/${prev.fields.slug}`}><ArrowLeft /></Link>}
+              {prev && (
+                <Link to={`/gallery/${prev.fields.slug}`}>
+                  <ArrowLeft />
+                </Link>
+              )}
             </div>
             <div className={styles.arrow}>
-              {next && <Link to={`/gallery/${next.fields.slug}`}><ArrowRight /></Link>}
+              {next && (
+                <Link to={`/gallery/${next.fields.slug}`}>
+                  <ArrowRight />
+                </Link>
+              )}
             </div>
           </div>
           <div>
-            <Img
-              className={styles.img}
-              fluid={
-                props.data.markdownRemark.frontmatter.featuredImage
-                  .childImageSharp.fluid
-              }
-            />
+                <Img
+            className={styles.img}
+            fluid={
+              props.data.markdownRemark.frontmatter.featuredImage
+                .childImageSharp.fluid
+            }
+          />
           </div>
+      
           <div className={styles.detail_info}>
             <div
               className={styles.description}
